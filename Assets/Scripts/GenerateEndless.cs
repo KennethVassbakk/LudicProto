a89e1e87ad1f9cdce799b820863e1ada062f9bad
MrSpectacle <kenneth.vassbakk@gmail.com>
2020-02-26 13:04:10 +0100

    Endless Generation of ground completed.

--- Assets/Scripts/GenerateEndless.cs
+++ Assets/Scripts/GenerateEndless.cs
@@ -1,40 +1,53 @@
-﻿using System.Collections;
+﻿/**
+ *  Author: Kenneth Vassbakk 
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GenerateEndless : MonoBehaviour
 {
     EndlessProperties _gm;
-    // Start is called before the first frame update
+    
     void Start()
     {
         _gm = GameObject.Find("_GM").GetComponent<EndlessProperties>();
-    
-        if(_gm.preGen > 0) {
-            preGen();
+        preGen(_gm.tileLength);
+    }
+
+    /// <summary>
+    /// Just itterates through Generate the amount of times needed to preload the map.
+    /// </summary>
+    /// <param name="tileLength"></param>
+    public void preGen(int tileLength) {
+        for (int i = 0; i < tileLength; i++) {
+            Generate(new Vector3(0, 0, (int)(i * _gm.tileSize)), true);
         }
     }
 
-
-
-    public void preGen() {
-        for (int i = 0; i < _gm.preGen; i++) {
-            Generate(new Vector3(0, 0, (int)(i * _gm.tileSize)));
-        }
-    }
-
+    /// <summary>
+    /// Generate environment ahead of the player.
+    /// Pregen generates a load at the beginning. 
+    /// </summary>
+    /// <param name="position">Vector 3 position</param>
+    /// <param name="pregen">True or false, default false.</param>
     public void Generate(Vector3 position, bool pregen = false) {
         GameObject center = _gm.ground[Random.Range(0, _gm.ground.Length)];
 
         int location = (int)position.z + (_gm.tileSize * _gm.tileLength);
-        PoolManager.Spawn(center, new Vector3(0, 0, pregen ? location : position.z), Quaternion.identity);
+        PoolManager.Spawn(center, new Vector3(0, 0, pregen ? position.z : location), Quaternion.identity);
     }
 
+    /// <summary>
+    /// When an environment item colliders with the trigger, it will despawn it and spawn a new set further ahead.
+    /// saves memory.
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other) {
-        Debug.Log("Trigger entered!");
         if (other.gameObject.tag == "Env") {
-            Generate(other.gameObject.transform.parent.gameObject.transform.position);
+            Vector3 position = other.gameObject.transform.position;
             PoolManager.Despawn(other.gameObject);
+            Generate(position);
         }
     }
 }
