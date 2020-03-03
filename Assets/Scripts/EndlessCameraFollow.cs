917f606e319357bc51e1cd3f2ae8c2d058ba2066
MrSpectacle <kenneth.vassbakk@gmail.com>
2020-03-03 10:51:27 +0100

    Camera Smoothing

--- Assets/Scripts/EndlessCameraFollow.cs
+++ Assets/Scripts/EndlessCameraFollow.cs
@@ -6,13 +6,20 @@ using UnityEngine;
 public class EndlessCameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float smooth = 0.1f;
+    public float smooth = 5f;
     public Vector3 cameraOffset;
 
     private void Update() {
-        Vector3 desiredPos = target.position + cameraOffset;
-        Vector3 smoothPos = Vector3.Lerp(transform.position, desiredPos, smooth);
+        Vector3 desiredPos = transform.position;
+        if (target.position.x < -0.1f) {
+            desiredPos = new Vector3(-1f, transform.position.y, transform.position.z);
+        } else if (target.position.x > -0.1f & target.position.x < 0.1f) {
+            desiredPos = new Vector3(0, transform.position.y, transform.position.z);
+        } else if (target.position.x > 0.1f) {
+            desiredPos = new Vector3(1f, transform.position.y, transform.position.z);
+        }
+        Vector3 smoothPos = Vector3.Lerp(transform.position, desiredPos, smooth * Time.deltaTime);
 
-        transform.position = desiredPos;
+        transform.position = smoothPos;
     }
 }
