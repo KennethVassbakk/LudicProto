c5d58913b1e57aba818baefd08f4d95fd27164b0
MrSpectacle <kenneth.vassbakk@gmail.com>
2020-03-04 14:29:15 +0100

    Start of audio input

---
+++ Assets/Scripts/Endless_Story.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endless_Story : MonoBehaviour
+{
+
+    public AudioClip startAudio;
+    public AudioClip failAudio;
+    public AudioClip sucessAudio;
+
+    public GameObject player;
+    public GameObject enemy;
+
+    private AudioSource _as;
+
+    private bool started = false;
+
+
+    private void Start() {
+        _as = GetComponent<AudioSource>();
+        _as.clip = startAudio;
+
+        _as.Play();
+    }
+
+    public void startGame() {
+        Debug.Log("STARTING!");
+        started = true;
+        player.GetComponent<test_runner>().keepMoving = true;
+        enemy.GetComponent<EnemyRunner>().keepMoving = true;
+        _as.Stop();
+
+    }
+}
