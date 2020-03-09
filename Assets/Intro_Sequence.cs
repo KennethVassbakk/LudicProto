ae8b00328cc55bc6038dbf8c5da152b58e1b2be8
MrSpectacle <kenneth.vassbakk@gmail.com>
2020-03-09 12:58:34 +0100

    Intro Beginning

---
+++ Assets/Intro_Sequence.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro_Sequence : MonoBehaviour
+{
+    public GameObject Hints;
+    public void DisableAnimator()
+    {
+        this.GetComponent<Animator>().enabled = false;
+        GiveHints();
+    }
+
+    public void GiveHints()
+    {
+        Hints.SetActive(true);
+    }
+}
--- Assets/Scripts/EndlessCameraFollow.cs
+++ Assets/Scripts/EndlessCameraFollow.cs
@@ -12,16 +12,21 @@ public class EndlessCameraFollow : MonoBehaviour
     public Vector3 cameraOffset;
     public float minX = -1f;
     public float maxX = 1f;
+    public bool minMax = true;
 
     private void LateUpdate() {
         Vector3 desiredPos = target.position + cameraOffset;
         Vector3 smoothPos = Vector3.Lerp(transform.position, desiredPos, smooth);
 
-        // Set limits of position
-        if(smoothPos.x <= minX) {
-            smoothPos.x = minX;
-        } else if (smoothPos.x >= maxX) {
-            smoothPos.x = maxX;
+        if (minMax)
+        {
+            // Set limits of position
+            if (smoothPos.x <= minX) {
+                smoothPos.x = minX;
+            } else if (smoothPos.x >= maxX) {
+                smoothPos.x = maxX;
+            }
+
         }
 
 
