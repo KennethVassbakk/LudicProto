2caa0827fdae8b13f5a6333ba6b2c03423ea91f4
MrSpectacle <kenneth.vassbakk@gmail.com>
2020-03-11 11:14:21 +0100

    GameProperties!

---
+++ Assets/Scripts/CheckGameProperties.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckGameProperties : MonoBehaviour
+{
+    public Text Shoes;
+    public Text Scarf;
+
+    public void Start()
+    {
+        CheckProps();
+    }
+
+    public void CheckProps()
+    {
+        Shoes.text = GameProperties.HaveShoes.ToString();
+        Scarf.text = GameProperties.HaveScarf.ToString();
+    }
+
+    public void SetScarf()
+    {
+        GameProperties.HaveScarf = 1;
+        CheckProps();
+    }
+
+    public void SetShoes()
+    {
+        GameProperties.HaveShoes = 1;
+        CheckProps();
+    }
+}
---
+++ Assets/Scripts/GameProperties.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public  static class GameProperties
+{
+    public static int HaveShoes { get; set; } = 0;
+
+    public static int HaveScarf { get; set; } = 0;
+}
