48fda1974fee5aecedb32901081049a2150bb2eb
JohnH <51337462+JohnHauge@users.noreply.github.com>
2020-03-18 17:15:25 +0100

    Wind and song etc

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
